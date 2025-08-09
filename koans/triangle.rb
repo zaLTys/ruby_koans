eada01b74152fda8516d0a98814873ed1edcc16b
Povilas Simanskas <povilas.simanskas@vinted.com>
2025-08-09 21:16:12 +0300

    exceptions meh, control interesting, triangle exceptions interesting

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,8 +14,34 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+
+ if [a, b, c].all? { |side| side == 0 }
+  raise TriangleError, "0 sides"
 end
+
+if [a, b, c].any? { |side| side < 0 }
+  raise TriangleError, "negative side"
+end
+
+
+arr = [a, b, c]
+if arr.any? { |side| side >= (arr.sum - side) }
+  raise TriangleError, "not a triangle"
+end
+
+  if a == b  && b == c
+    :equilateral
+  elsif a != b && b != c && a != c
+    :scalene
+  else  
+   :isosceles   
+  end
+end
+
+#def triangle(a, b, c)
+#  [a, b, c].uniq.length == 1 ? :equilateral : [a, b, c].uniq.length == 2 ? :isosceles : :scalene
+#end
+
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
