21d339ca4100ef2e89b8215387038b7d0142c875
Povilas Simanskas <povilas.simanskas@vinted.com>
2025-08-10 21:18:54 +0300

    modules and scopes are nice, inheritance syntax meh

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -2,9 +2,18 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+
+  @rolls=[]
+  
+  def roll(number_of_rolls)
+   @rolls = Array.new(number_of_rolls) { rand(1..6) }
+  end
+
+  def values
+    @rolls
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
